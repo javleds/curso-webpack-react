2de3d70f1831412d93de3abe919fe7c4219d4a5d
Javier Ledezma <juls0593@gmail.com>
2021-04-07 21:36:42 -0500

    Add html support to see the content with webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
 
 module.exports = {
     entry: './src/index.js',
@@ -14,12 +15,28 @@ module.exports = {
             {
                 test: /\.(js|jsx)$/,
                 exclude: /node_modules/,
-                use: {
-                    loader: "babel-loader"
-                }
+                use: [
+                    {
+                        loader: "babel-loader"
+                    }
+                ],
+            },
+            {
+                test: /\.html$/,
+                use: [
+                    {
+                        loader: "html-loader"
+                    }
+                ]
             }
         ]
     },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: "./public/index.html",
+            filename: "index.html"
+        })
+    ],
     devServer: {
         contentBase: path.resolve(__dirname, 'dist'),
         compress: true,
