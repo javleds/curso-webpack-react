2d056c85ee60edb7f4d0fcaf1fe4e00285e9b48a
Javier Ledezma <juls0593@gmail.com>
2021-04-07 21:43:12 -0500

    Add sass support

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require('path')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 
 module.exports = {
     entry: './src/index.js',
@@ -28,6 +29,14 @@ module.exports = {
                         loader: "html-loader"
                     }
                 ]
+            },
+            {
+                test: /\.s[ac]ss$/,
+                use: [
+                    'style-loader',
+                    'css-loader',
+                    'sass-loader'
+                ]
             }
         ]
     },
@@ -35,7 +44,10 @@ module.exports = {
         new HtmlWebpackPlugin({
             template: "./public/index.html",
             filename: "index.html"
-        })
+        }),
+        new MiniCssExtractPlugin({
+            filename: '[name].css'
+        }),
     ],
     devServer: {
         contentBase: path.resolve(__dirname, 'dist'),
